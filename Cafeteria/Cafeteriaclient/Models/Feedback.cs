f68b261134ad98b8033a3e09229e15e412a4c35f
dushyantraj <125907422+dushyantraj@users.noreply.github.com>
2024-07-04 00:01:21 +0530

    Delete Cafeteria directory

--- Cafeteria/Cafeteriaclient/Models/Feedback.cs
+++
@@ -1,9 +0,0 @@
-namespace CafeteriaClient.Models
-{
-    public class Feedback
-    {
-        public string ItemName { get; set; }
-        public int Rating { get; set; }
-        public string Comments { get; set; }
-    }
-}
